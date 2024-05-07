e80207ad237868c9852c05927ff46646fceab6f5
Anish Kasam <anishkasam@gmail.com>
2024-05-07 15:34:49 -0700

    bag talk

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -12,15 +12,36 @@
   // Set initial year
   let selectedYear = 2003;
   let selectedType = "Total Yards";
+  let lowercaseType = "total yards";
   const yardTypes = ["Total Yards", "Passing Yards", "Rushing Yards"];
+
   let dataForYear = data.filter(entry => entry.year === selectedYear);
+  dataForYear.sort((a, b) => a.team.localeCompare(b.team));
+
+  let average = dataForYear.reduce((acc, cur) => acc + cur.rush_yds + cur.pass_yds, 0) / 32;
 
   // Function to update chart data when year changes
   function updateChartData(year, type) {
     selectedYear = year;
     selectedType = type;
+    lowercaseType = selectedType.toLowerCase();
+
+    // sort teams alphabetically so order is preserved
     dataForYear = data.filter(entry => entry.year === year);
+    dataForYear.sort((a, b) => a.team.localeCompare(b.team));
+
+    if (selectedType == "Total Yards") {
+      average = dataForYear.reduce((acc, cur) => acc + cur.rush_yds + cur.pass_yds, 0) / 32;
+    }
+    else if (selectedType == "Passing Yards") {
+      average = dataForYear.reduce((acc, cur) => acc + cur.pass_yds, 0) / 32;
+    }
+    else {
+      average = dataForYear.reduce((acc, cur) => acc + cur.rush_yds, 0) / 32;
+    }
+
     dispatch('updateChartData', dataForYear);
+    console.log(dataForYear);
   }
 
   let intervalId;
@@ -48,24 +69,27 @@
 <main>
   <div class="container">
     <h1>How has Passing & Rushing Changed in the NFL in the Past 20 Years?</h1>
-    <select bind:value={selectedYear} on:change={() => updateChartData(selectedYear, selectedType)}>
-      {#each years as year}
-        <option value={year}>{year}</option>
-      {/each}
-    </select>
-    <select bind:value={selectedType} on:change={() => updateChartData(selectedYear, selectedType)}>
-      {#each yardTypes as type}
-        <option value={type}>{type}</option>
-      {/each}
-    </select>
-    <div>
+    <div class="controls">
+      <select bind:value={selectedYear} on:change={() => updateChartData(selectedYear, selectedType)}>
+        {#each years as year}
+          <option value={year}>{year}</option>
+        {/each}
+      </select>
       {#if playing}
         <button on:click={togglePlay}>Stop</button>
       {:else}
         <button on:click={togglePlay}>Play</button>
       {/if}
+      <select bind:value={selectedType} on:change={() => updateChartData(selectedYear, selectedType)}>
+        {#each yardTypes as type}
+          <option value={type}>{type}</option>
+        {/each}
+      </select>
     </div>
-    <BarChart data={dataForYear} type={selectedType}/>
+    <h3>In {selectedYear}, each team gained an average of {Math.round(average)} {lowercaseType}</h3>
+    <div class="chartContainer">
+      <BarChart data={dataForYear} type={selectedType}/>
+    </div>
   </div>
 </main>
 
@@ -76,13 +100,22 @@
     font-weight: 400;
   }
 
+  h3 {
+    font-family: "Inter";
+    margin-top: 100px;
+    font-weight: 400;
+  }
+
+  .chartContainer {
+    margin-top: -125px;
+  }
+
   select {
     font-family: "Inter";
     font-size: 16px;
-    padding: 8px;
+    padding-top: -10px;
     border-radius: 5px;
     border: 1px solid #ccc;
-    margin-right: 10px;
   }
 
   .container {
@@ -91,15 +124,28 @@
     text-align: center;
   }
 
-  button {
+  .controls {
+    margin-top: 25px;
+  }
+
+  .controls *  {
+    display: inline-flex;
     font-family: "Inter";
     font-size: 16px;
-    padding: 8px 16px;
+    padding-top: -10px;
     border-radius: 5px;
-    border: none;
-    background-color: #007bff;
-    color: #fff;
+    border: 1px solid #ccc;
     cursor: pointer;
-    margin: 10px;
+    background: white;
+    padding: 10px 15px 10px 15px;
+    margin-left: 5px;
+    margin-right: 5px;
+    text-align: center;
+  }
+
+  .controls button {
+    width: 80px;
+    display: inline-block;
+    text-align: center;
   }
 </style>
--- src/components/BarChart.svelte
+++ src/components/BarChart.svelte
@@ -14,10 +14,12 @@
   
       // Define chart dimensions
       const width = window.innerWidth * 0.8;
-      const height = 700;
-      const margin = { top: 70, right: 70, bottom: 200, left: 120 };
-      const innerWidth = width - margin.left - margin.right;
+      const height = window.innerHeight * 0.8;
+      const margin = { top: 70, right: 100, bottom: 200, left: 100};
+      const innerWidth = width - 100;
       const innerHeight = height - margin.top - margin.bottom;
+      const rushColor = '#eb4d4b';
+      const passColor = '#22a6b3';
   
       // Create SVG element
       const svg = d3.select('.chart-container')
@@ -49,7 +51,7 @@
             .attr('y', d => yScale(d.pass_yds + d.rush_yds))
             .attr('width', xScale.bandwidth())
             .attr('height', d => innerHeight - yScale(d.pass_yds))
-            .attr('fill', 'steelblue');
+            .attr('fill', passColor);
 
          // Create and append bars for rushing yards
          svg.selectAll('.rushing-bar')
@@ -61,7 +63,7 @@
             .attr('y', d => yScale(d.rush_yds))
             .attr('width', xScale.bandwidth())
             .attr('height', d => yScale(d.pass_yds) - yScale(d.pass_yds + d.rush_yds))
-            .attr('fill', 'green');
+            .attr('fill', rushColor);
       }
 
       if (type == "Passing Yards") {
@@ -75,7 +77,7 @@
             .attr('y', d => yScale(d.pass_yds))
             .attr('width', xScale.bandwidth())
             .attr('height', d => yScale(d.rush_yds) - yScale(d.pass_yds + d.rush_yds))
-            .attr('fill', 'steelblue');
+            .attr('fill', passColor);
       }
       
       if (type == "Rushing Yards") {
@@ -89,7 +91,7 @@
             .attr('y', d => yScale(d.rush_yds))
             .attr('width', xScale.bandwidth())
             .attr('height', d => yScale(d.pass_yds) - yScale(d.pass_yds + d.rush_yds))
-            .attr('fill', 'green');
+            .attr('fill', rushColor);
       }
   
       // Create X axis
@@ -100,6 +102,7 @@
          .selectAll('text')
          .attr('transform', 'rotate(-45)')
          .style('text-anchor', 'end')
+         .attr('font-family', 'Inter')
          .attr('font-size', '16px');
   
       // Create Y axis
@@ -107,26 +110,29 @@
          .attr('class', 'y-axis')
          .call(d3.axisLeft(yScale).ticks(10))
          .selectAll('text')
+         .attr('font-family', 'Inter')
          .attr('font-size', '16px');
   
       // Add X axis label
       svg.append('text')
          .attr('class', 'x-axis-label')
          .attr('x', innerWidth / 2)
-         .attr('y', innerHeight + margin.bottom * 1)
+         .attr('y', innerHeight + margin.bottom * 0.9)
          .attr('text-anchor', 'middle')
-         .text('NFL Teams')
-         .attr('font-size', '20px');
+         .text('Team Name')
+         .attr('font-family', 'Inter')
+         .attr('font-size', '24px');
   
       // Add Y axis label
       svg.append('text')
          .attr('class', 'y-axis-label')
          .attr('transform', 'rotate(-90)')
          .attr('x', -innerHeight / 2)
-         .attr('y', -margin.left / 2)
+         .attr('y', -margin.left / 1.2)
          .attr('text-anchor', 'middle')
          .text('Yards')
-         .attr('font-size', '20px');
+         .attr('font-family', 'Inter')
+         .attr('font-size', '24px');
     }
     
       
@@ -145,6 +151,20 @@
   
   <div class="chart-container"></div>
   
-  <style>
-    /* Add your CSS styles here */
-  </style>
+   <style>
+      .svg-container {
+         display: inline-block;
+         position: relative;
+         width: 100%;
+         padding-bottom: 100%; /* aspect ratio */
+         vertical-align: top;
+         overflow: hidden;
+      }
+
+      .svg-content-responsive {
+         display: inline-block;
+         position: absolute;
+         top: 10px;
+         left: 0;
+      }
+   </style>
